4aa047059728daac5d733d9abefaf281b0a64ba6
Vr3n <vickyspatel@gmail.com>
2023-12-09 15:01:54 +0530

    Implemented Images and Scrolling in Location Detail Page.
    TODO: Implement Gesture Controls for Scrolling.

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,7 +1,9 @@
 import { ArrowCircleRight } from "@phosphor-icons/react";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import KioskButton from "~/components/KioskButton/KioskButton";
 import { Separator } from "~/components/ui/separator";
+import { ScrollArea, ScrollBar } from "~/components/ui/scroll-area";
 import KioskLocationBaseLayout from "~/layouts/KioskLocationBaseLayout";
 
 const LocationDetailPage = () => {
@@ -13,10 +15,29 @@ const LocationDetailPage = () => {
         <h1 className="text-4xl font-bold">{router.query.slug}</h1>
         <Separator className="my-4 border border-zinc-400" />
         <p className="text-2xl font-semibold">Images</p>
-        <Separator className="my-4 border border-zinc-200" />
-        <p className="text-2xl font-semibold">Journals</p>
-        <Separator className="my-4 border border-zinc-200" />
-        <p className="text-2xl font-semibold">News</p>
+        <ScrollArea className="mt-4">
+          <div className="flex w-max gap-4">
+            {[100, 232, 311, 34, 445, 116, 27, 88].map((i) => (
+              <figure key={i} className="shrink-0">
+                <div className="overflow-hidden rounded-md">
+                  <Image
+                    className="border-zinc-200"
+                    src={`https://picsum.photos/id/${i}/200`}
+                    height={200}
+                    width={200}
+                    alt="Photo"
+                  />
+                </div>
+              </figure>
+            ))}
+          </div>
+          <ScrollBar
+            className="h-6 rounded-md bg-zinc-600 transition-all delay-150"
+            orientation="horizontal"
+          />
+        </ScrollArea>
+        <p className="mt-4 text-2xl font-semibold">Journals</p>
+        <p className="mt-4 text-2xl font-semibold">News</p>
       </div>
       <footer className="mt-6">
         <div className="flex justify-between">
